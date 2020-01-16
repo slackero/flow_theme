59a099c59c8d7b74461e462089062ebbb61b21fd
slackero <slackero@gmail.com>
2020-01-16 15:40:30 +0300

    Increase autoprefixer browser compatibility, optimise clean css

--- grunt/less.js
+++ grunt/less.js
@@ -2,10 +2,10 @@ module.exports = {
 
     options: {
         compress: true,
-        yuicompress: true,
         optimization: 2,
         plugins: [
-            new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]})
+            new (require('less-plugin-autoprefix'))({browsers: ["last 6 versions", ">0.2%", "Firefox ESR", "not dead"]}),
+            new (require('less-plugin-clean-css'))()
         ]
     },
 
